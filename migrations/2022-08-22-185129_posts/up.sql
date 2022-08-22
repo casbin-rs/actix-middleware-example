cd34f5abae3b855e83d45f5df434751f5769e9ca
Siddhesh Kanawade <siddhesh.kanawade@iitgn.ac.in>
2022-08-23 00:49:04 +0530

    feat: diesel setup, implemented db, authn middleware template

---
+++ migrations/2022-08-22-185129_posts/up.sql
@@ -0,0 +1,10 @@
+-- Your SQL goes here
+CREATE TABLE posts
+(
+    id         SERIAL       NOT NULL PRIMARY KEY,
+    title      VARCHAR(200) NOT NULL,
+    body       TEXT         NOT NULL,
+    is_deleted BOOLEAN      NOT NULL DEFAULT 'f',
+    created_at TIMESTAMP    NOT NULL,
+    deleted_at TIMESTAMP
+)
--- src/middleware/authn.rs
+++ src/middleware/authn.rs
@@ -1,1 +1,69 @@
+#![allow(clippy::type_complexity)]
 
+use actix_casbin_auth::CasbinVals;
+use actix_service::{Service, Transform};
+use actix_web::{
+    body::MessageBody,
+    dev::{ServiceRequest, ServiceResponse},
+    Error, HttpMessage,
+};
+use futures::{
+    future::{ok, Ready},
+    Future,
+};
+use std::cell::RefCell;
+use std::rc::Rc;
+use std::{
+    pin::Pin,
+    task::{Context, Poll},
+};
+
+pub struct Authentication;
+
+impl<S, B> Transform<S, ServiceRequest> for Authentication
+where
+    S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error> + 'static,
+    B: MessageBody,
+{
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type InitError = ();
+    type Transform = AuthenticationMiddleware<S>;
+    type Future = Ready<Result<Self::Transform, Self::InitError>>;
+
+    fn new_transform(&self, service: S) -> Self::Future {
+        ok(AuthenticationMiddleware {
+            service: Rc::new(RefCell::new(service)),
+        })
+    }
+}
+pub struct AuthenticationMiddleware<S> {
+    service: Rc<RefCell<S>>,
+}
+
+impl<S, B> Service<ServiceRequest> for AuthenticationMiddleware<S>
+where
+    S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error> + 'static,
+    B: MessageBody,
+{
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>>>>;
+
+    fn poll_ready(&self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.service.poll_ready(cx)
+    }
+
+    fn call(&self, req: ServiceRequest) -> Self::Future {
+        let svc = self.service.clone();
+
+        Box::pin(async move {
+            let vals = CasbinVals {
+                subject: String::from("alice"),
+                domain: None,
+            };
+            req.extensions_mut().insert(vals);
+            svc.call(req).await
+        })
+    }
+}
